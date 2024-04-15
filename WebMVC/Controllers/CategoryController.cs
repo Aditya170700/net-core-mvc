c4d2ec57199af721851326097a9b69ef994bb5a7
Aditya170700 <adityaric72@gmail.com>
2024-04-16 05:41:14 +0700

    Category list

--- WebMVC/Controllers/CategoryController.cs
+++ WebMVC/Controllers/CategoryController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebMVC.Data;
+using WebMVC.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,19 @@ namespace WebMVC.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _appDbContext;
+
+        public CategoryController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            List<Category> results = _appDbContext.Categories.ToList();
+
+            return View(results);
         }
     }
 }
