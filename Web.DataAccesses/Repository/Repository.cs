b5f310f79ac1d74d44f1a056b3e3a5a459a440c1
Aditya170700 <adityaric72@gmail.com>
2024-04-18 06:42:42 +0700

    setup repository pattern

---
+++ Web.DataAccesses/Repository/Repository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Web.DataAccesses.Data;
+using Web.DataAccesses.Repository.IRepository;
+namespace Web.DataAccesses.Repository
+{
+	public class Repository<T> : IRepository<T> where T : class
+	{
+        private readonly AppDbContext _appDbContext;
+        internal DbSet<T> _dbSet;
+
+        public Repository(AppDbContext appDbContext)
+		{
+            _appDbContext = appDbContext;
+            _dbSet = _appDbContext.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = _dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = _dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+    }
+}
+
