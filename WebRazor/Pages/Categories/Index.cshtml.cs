b87cb5b7b00ac507132971fd448f5775b43f014b
Aditya170700 <adityaric72@gmail.com>
2024-04-17 18:41:07 +0700

    Get all category in razor page

---
+++ WebRazor/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebRazor.Data;
+using WebRazor.Models;
+
+namespace WebRazor.Pages.Categories
+{
+	public class IndexModel : PageModel
+    {
+        private readonly AppDbContext _appDbContext;
+        public List<Category> Categories { get; set; }
+
+        public IndexModel(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public void OnGet()
+        {
+            Categories = _appDbContext.Categories.ToList();
+        }
+    }
+}
