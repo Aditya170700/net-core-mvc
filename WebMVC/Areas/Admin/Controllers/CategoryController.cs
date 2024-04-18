87a1c77a57514ec3fa75a7268e4215f9c261cb62
Aditya170700 <adityaric72@gmail.com>
2024-04-19 06:25:13 +0700

    Add area admin & customer

--- WebMVC/Areas/Admin/Controllers/CategoryController.cs
+++ WebMVC/Areas/Admin/Controllers/CategoryController.cs
@@ -9,8 +9,9 @@ using Web.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace WebMVC.Controllers
+namespace WebMVC.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
