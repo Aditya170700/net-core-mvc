eba561b675c12a36285e528a99e1cee1734161d9
Aditya170700 <adityaric72@gmail.com>
2024-04-21 16:43:36 +0700

    Product category relationship

--- Web.Models/Product.cs
+++ Web.Models/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Web.Models
 {
@@ -36,6 +37,10 @@ namespace Web.Models
         [DisplayName("Price for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
     }
 }
 
